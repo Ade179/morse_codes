a7a1ae25398eaf0169262b56e7bc24b47ffbd9a2
JacintaNgwu <jacintangwu9900@gmail.com>
2023-03-09 15:35:33 +0530

    Created a method to decode entire message

--- decode_char.rb
+++ decode_char.rb
@@ -6,7 +6,6 @@ morse_char = {
   '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
 }
 
-
 def decode_chars(char)
   morse_char[char]
 end
@@ -14,3 +13,8 @@ end
 def decode_word(words)
   words.split.map { |char| decode_chars(char) }.join
 end
+
+def decode_message(message)
+    message.strip.split('   ').map { |message| decode_word(message) }.join(' ')
+  end
+  puts decode_message('-- -.--   -. .- -- .')
